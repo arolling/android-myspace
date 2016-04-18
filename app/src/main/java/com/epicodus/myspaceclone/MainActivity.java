2e38bfbdb3f7936e63fb3809c5037f41ec464d18
Abigail Rolling and Brad London <arolling@gmail.com>
2016-04-18 13:36:45 -0700

    Use picasso to add remote image as background

--- app/src/main/java/com/epicodus/myspaceclone/MainActivity.java
+++ app/src/main/java/com/epicodus/myspaceclone/MainActivity.java
@@ -2,6 +2,10 @@ package com.epicodus.myspaceclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.content.Context;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String imageUri = "https://images.unsplash.com/photo-1446426156356-92b664d86b77?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=92e96949ac731e7cbcac943aa5934554";
+        ImageView ivBackgroundImage = (ImageView) findViewById(R.id.imageViewBackground);
+        Picasso.with(this).load(imageUri).into(ivBackgroundImage);
     }
 }
